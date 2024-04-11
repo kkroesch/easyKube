a543422c76cc4e559662ec88580540591ab6d418
Karsten Kroesch <karsten.kroesch@ag.ch>
2024-04-11 15:24:09 +0200

    Added variables

---
+++ variables.tf
@@ -0,0 +1,28 @@
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "namespaces" {
+  description = "List of Namespaces"
+  type        = list(string)
+  default     = []
+}
+
+variable "cloudflare_email" {
+  type        = string
+  description = "Die E-Mail-Adresse für das Cloudflare-Konto"
+}
+
+variable "cloudflare_api_key" {
+  type        = string
+  description = "Der API-Schlüssel für das Cloudflare-Konto"
+}
+
+variable "cloudflare_zone_id" {
+  type        = string
+  description = "Die Zone ID deiner Domain bei Cloudflare"
+}
+
+
